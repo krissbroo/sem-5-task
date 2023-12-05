b1ee3e442713c010e1150751dfa83cae71990def
krissbroo <147873888+krissbroo@users.noreply.github.com>
2023-12-05 23:49:32 +0800

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -1,5 +1,9 @@
-﻿//  Напишите программу, которая на вход принимает позиции элемента в двумерном массиве, 
-//  и возвращает значение этого элемента или же указание, что такого элемента нет.
+﻿// Задайте двумерный массив. Напишите программу, которая поменяет местами первую и последнюю строку массива.
+// 4 3 1 => 4 6 2
+// 2 6 9    2 6 9
+// 4 6 2    4 3 1
+
+
 
 int ReadInt(string text)
 {
@@ -34,23 +38,34 @@ System.Console.WriteLine();
 }
 }
 
-// ------------------------------------------
+
 
 int rows = ReadInt("Введите количество строк массива: ");
 int cols = ReadInt("Введите количество столбцов массива: ");
-int row = ReadInt("введите номер строки массива ");
-int col = ReadInt("введите номер столбца массива ");
+
 // Заполнение массива
 int[,] matrix = GenerateMatrix(rows, cols, 1, 10);
 
 PrintMatrix(matrix);
+Console.WriteLine(); 
 
 
-   if (row > 0 | row < matrix.GetLength(0) - 1 | col > 0 | col < matrix.GetLength(1) - 1)
-{
-Console.WriteLine("Значение элемента массива = {0} ", matrix[row,col]);
-}
-    else
-{
-    Console.WriteLine("Элемент не существует  ");
-}
+for(int i = 0; i < matrix.GetLength(1); i++)
+            {
+                int change = matrix[matrix.GetLength(0) -1 , i];
+                matrix[matrix.GetLength(0) -1, i] = matrix[0, i];
+                matrix[0, i] = change;
+            }
+            for(int i = 0; i < matrix.GetLength(0); i++)
+            {
+                for(int j = 0; j < matrix.GetLength(1); j++)
+                {
+                    Console.Write(matrix[i,j] + "\t");
+                }
+                Console.WriteLine();
+            }
+
+
+
+        //  не могу понять как определить это действие в функцию 
+        
